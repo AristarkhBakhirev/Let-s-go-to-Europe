fcb725c264b30f2438d8831685331b1fcda877a1
Bakhirev Aristarkh <bahirev94@bk.ru>
2021-04-26 17:49:54 +0300

    Global update with a complete change of the project structure (now build on Golang, and PHP)

---
+++ other/drafts/localhost.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type msg string
+
+func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(resp, m)
+}
+
+func main() {
+	msgHandler := msg("...")
+	fmt.Println("Server is listening...")
+	http.ListenAndServe("localhost:8181", msgHandler)
+}
